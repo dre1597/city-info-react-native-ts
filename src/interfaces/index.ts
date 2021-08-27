d177589c193b95f4dedbada07491a1e8a186d353
André Pereira <andrepereira97@yahoo.com>
2021-08-27 11:57:15 -0300

    Api data interface

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export interface ICityInfo {
+    city: string;
+    state: string;
+    country: string;
+    drive_on: string;
+    speed_in: string;
+    currency_name: string;
+    currency_symbol: string;
+}
